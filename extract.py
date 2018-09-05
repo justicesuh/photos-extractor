c9dd7a16be4aedaf032614d2cfe9064399de1174
Justice Suh <justice.suh@gmail.com>
2018-09-05 18:31:07 -0400

    Get formats

--- extract.py
+++ extract.py
@@ -1,7 +1,20 @@
 from glob import glob
 import sys
 
-if __name__ == '__main__':
-    files = glob(sys.argv[1] + '/**', recursive=True)
+def get_formats(files):
+    '''
+    return list of formats present
+    '''
+    formats = []
     for file in files:
-        print(file)
+        if file.count('.') == 2:
+            ext = file[file.rindex('.') + 1:]
+            if ext not in formats:
+                formats.append(ext)
+    return formats
+
+if __name__ == '__main__':
+    files = map(str.lower, glob(sys.argv[1] + '/**', recursive=True))
+    formats = get_formats(files)
+    for fmt in formats:
+        print(fmt)
