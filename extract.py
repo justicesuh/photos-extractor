a759f771feb73e8ebe0fb0b532fe1a774fe32f93
Justice Suh <justice.suh@gmail.com>
2018-09-05 19:33:54 -0400

    Copy PhotoBooth media

--- extract.py
+++ extract.py
@@ -1,18 +1,32 @@
 from glob import glob
+import os
 import sys
+from shutil import copy2
+
+def create_folders(*folders):
+    '''
+    create folders if they don't already exist
+    '''
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
 if __name__ == '__main__':
-    formats = ['jpeg', 'jpg', 'raw', 'png', 'gif', 'tiff', 'mp4', 'mov']
-    files = sum([glob(sys.argv[1] + '/**/*.' + f, recursive=True) for f in formats], [])
+    src = sys.argv[1]
+    dst = sys.argv[2]
+    photobooth = '{}/PhotoBooth'.format(dst)
+    library = '{}/Library'.format(dst)
 
-    folders = []
+    create_folders(dst, photobooth, library)
+
+    # get all media files
+    formats = ['jpeg', 'jpg', 'raw', 'png', 'gif', 'tiff', 'mp4', 'mov']
+    files = sum([glob('{}/**/*.{}'.format(src, f), recursive=True) for f in formats], [])
+
     for file in files:
+        # Library media
         if 'photoslibrary' in file:
             file = file[file.index('photoslibrary') + 14:]
-        else:
-            file = file[file.index('Library') + 8:]
-        folder = file[:file.index('\\')]
-        if folder not in folders:
-            folders.append(folder)
-    for folder in folders:
-        print(folder)
+        # PhotoBooth media
+        elif 'Library' in file:
+            copy2(file, photobooth)
