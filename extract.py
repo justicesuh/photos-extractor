4ae60a01bfc5cce3ddfbe48f54bcbb44da035c11
Justice Suh <justice.suh@gmail.com>
2018-09-05 21:38:00 -0400

    Copy without overwriting files

--- extract.py
+++ extract.py
@@ -1,7 +1,22 @@
 from glob import glob
 import os
-import sys
 from shutil import copy2
+import sys
+
+def copy(src, dst):
+    '''
+    copy file from src to dst without overwriting existing files
+    '''
+    filename = src.rsplit(os.sep, 1)[-1]
+    if os.path.exists(os.path.join(dst, filename)):
+        print('{} exists'.format(filename))
+        name, ext = filename.rsplit('.', 1)
+        count = 2
+        while os.path.exists(os.path.join(dst, '{} ({}).{}'.format(name, count, ext))):
+            count += 1
+        copy2(src, os.path.join(dst, '{} ({}).{}'.format(name, count, ext)))
+    else:
+        copy2(src, dst)
 
 if __name__ == '__main__':
     src = sys.argv[1]
@@ -24,4 +39,4 @@ if __name__ == '__main__':
 
     for i, file in enumerate(files, 1):
         print('Copying item {} of {}'.format(i, num))
-        copy2(file, dst)
+        copy(file, dst)
