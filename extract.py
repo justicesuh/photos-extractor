e7061e24b5fd85368be398be3cf24468d06912be
Justice Suh <justice.suh@gmail.com>
2018-09-05 20:59:17 -0400

    Search only relevant directories

--- extract.py
+++ extract.py
@@ -1,32 +1,21 @@
 from glob import glob
 import os
 import sys
-from shutil import copy2
-
-def create_folders(*folders):
-    '''
-    create folders if they don't already exist
-    '''
-    for folder in folders:
-        if not os.path.exists(folder):
-            os.makedirs(folder)
 
 if __name__ == '__main__':
     src = sys.argv[1]
     dst = sys.argv[2]
-    photobooth = '{}/PhotoBooth'.format(dst)
-    library = '{}/Library'.format(dst)
 
-    create_folders(dst, photobooth, library)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
 
-    # get all media files
+    files = []
+    folders = ['Masters', 'Originals', 'Pictures']
     formats = ['jpeg', 'jpg', 'raw', 'png', 'gif', 'tiff', 'mp4', 'mov']
-    files = sum([glob('{}/**/*.{}'.format(src, f), recursive=True) for f in formats], [])
-
-    for file in files:
-        # Library media
-        if 'photoslibrary' in file:
-            file = file[file.index('photoslibrary') + 14:]
-        # PhotoBooth media
-        elif 'Library' in file:
-            copy2(file, photobooth)
+    for folder in folders:
+        for fmt in formats:
+            print('Searching {} for *.{}'.format(folder, fmt))
+            files.extend(glob('{}/**/{}/**/*.{}'.format(src, folder, fmt), recursive=True))
+    num = len(files)
+    print()
+    print('Found {} items'.format(num))
